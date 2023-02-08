21913368d675ab23c0688f25bd4f9abd13e0fcdc
LU SIFE <itslusife@gmail.com>
2023-02-08 13:31:43 -0700

    Update Swap.bat

--- Swap.bat
+++ Swap.bat
@@ -16,18 +16,22 @@ ping -n 2 -w 1 127.0.0.1 > NUL
 %SendKeys% "{TAB}{TAB}{TAB}{ENTER}"
 ping -n 1 -w 1 127.0.0.1 > NUL
 %SendKeys% " "
-ping -n 1 -w 1 127.0.0.1 > NUL
-%SendKeys% "{ENTER}"
 ping -n 2 -w 1 127.0.0.1 > NUL
+%SendKeys% "{ENTER}"
+ping -n 1 -w 1 127.0.0.1 > NUL
 start ms-settings:
 ping -n 2 -w 1 127.0.0.1 > NUL
 %SendKeys% "{TAB}{ENTER}"
 ping -n 2 -w 1 127.0.0.1 > NUL
-%SendKeys% "{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}"
-ping -n 1 -w 1 127.0.0.1 > NUL
-%SendKeys% "{ENTER}"
+%SendKeys% "{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{ENTER}"
 ping -n 1 -w 1 127.0.0.1 > NUL
 %SendKeys% "{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{ENTER}"
+ping -n 2 -w 1 127.0.0.1 > NUL
+%SendKeys% "{TAB}{ENTER}"
+ping -n 1 -w 1 127.0.0.1 > NUL
+%SendKeys% "{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{ENTER}"
+ping -n 0 -w 1 127.0.0.1 > NUL
+%SendKeys% "{DOWN}{DOWN}{ENTER}"
 ping -n 2 -w 1 127.0.0.1 > NUL
 %SendKeys% "{TAB}{ENTER}"
 
