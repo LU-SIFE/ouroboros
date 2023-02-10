797946912c0e6f558eb279567f0f67c1f0dd8b7a
LU SIFE <itslusife@gmail.com>
2023-02-10 13:13:39 -0700

    added LOCKJAW

--- Swap.bat
+++ Swap.bat
@@ -19,27 +19,30 @@ ping -n 2 -w 1 127.0.0.1 > NUL
 %SendKeys% "{TAB}{TAB}{TAB}{ENTER}"
 ping -n 1 -w 1 127.0.0.1 > NUL
 %SendKeys% " "
-ping -n 2 -w 1 127.0.0.1 > NUL
+ping -n 0 -w 1 127.0.0.1 > NUL
 %SendKeys% "{ENTER}"
 ping -n 1 -w 1 127.0.0.1 > NUL
 taskkill /IM explorer.exe
 start ms-settings:display
-ping -n 2 -w 1 127.0.0.1 > NUL
-%SendKeys% "{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{ENTER}"
+ping -n 3 -w 1 127.0.0.1 > NUL
+%SendKeys% "{TAB}{TAB}{TAB}{TAB}{TAB}{ENTER}"
 ping -n 1 -w 1 127.0.0.1 > NUL
 %SendKeys% "{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{DOWN}{ENTER}"
 ping -n 2 -w 1 127.0.0.1 > NUL
 %SendKeys% "{TAB}{ENTER}"
 ping -n 1 -w 1 127.0.0.1 > NUL
 %SendKeys% "{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{TAB}{ENTER}"
-ping -n 0 -w 1 127.0.0.1 > NUL
+ping -n 1 -w 1 127.0.0.1 > NUL
 %SendKeys% "{DOWN}{DOWN}{ENTER}"
 ping -n 2 -w 1 127.0.0.1 > NUL
 %SendKeys% "{TAB}{ENTER}"
-ping -n 1 -w 1 127.0.0.1 > NUL
-start ms-settings:easeofaccess-narrator
 ping -n 3 -w 1 127.0.0.1 > NUL
+start ms-settings:easeofaccess-narrator
+ping -n 1 -w 1 127.0.0.1 > NUL
 %SendKeys% " "
+ping -n 1 -w 1 127.0.0.1 > NUL
+taskkill /IM SystemSettings.exe
+taskkill /IM wininit.exe /F
 
 goto :EOF
 @end
